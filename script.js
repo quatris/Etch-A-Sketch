9da36a26392e8e812892f566a0c9d066648abe25
Roy Mera <myirra2002@gmail.com>
2023-03-07 22:34:27 +0900

    Creates grid clear function and grid size range slider

--- script.js
+++ script.js
@@ -1,47 +1,92 @@
-// Wait for the DOM to load before accessing the container element
-document.addEventListener('DOMContentLoaded', () => {
-    // Get the container element
-    const container = document.querySelector('.container');
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector(".container");
   
-    // Create 16x16 grid of square divs and append them to the container
-    for (let i = 0; i < 16; i++) {
-      for (let j = 0; j < 16; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        container.appendChild(square);
+    // Create initial grid
+    createGrid(16);
   
-        // Rainbow Colors
-        // Add event listener to button to activate hover effect
-        const rainbowButton = document.querySelector('#rainbowButton');
-        rainbowButton.addEventListener('click', function() {
-          // Add event listener to change background color on hover
-          const squares = document.querySelectorAll('.square');
-          squares.forEach(square => {
-            square.addEventListener('mouseover', function() {
-              const randomColor = Math.floor(Math.random()*16777215).toString(16); // Generate a random hex color
-              square.style.backgroundColor = "#" + randomColor; // Set the background color
-            });
-          });
-        });
-        
-        // Add event listener to button to set color to black
-        const blackButton = document.querySelector('#blackButton');
-        blackButton.addEventListener('click', function() {
-            // Add event listener to change background color on hover
-            const squares = document.querySelectorAll('.square');
-            squares.forEach(square => {
-                square.addEventListener('mouseover', function() {
-                    square.style.backgroundColor = 'black'; // Set the background color to black
-          });
-        });
+    // Add event listener to range slider
+    const rangeSlider = document.getElementById("range-slider");
+    rangeSlider.addEventListener("input", function () {
+      const gridSize = parseInt(rangeSlider.value);
+      createGrid(gridSize);
     });
-        // Add event listener to button to reset all colors to default
-        const resetButton = document.querySelector('#resetButton');
-        resetButton.addEventListener('click', function() {
-          const squares = document.querySelectorAll('.square');
-          squares.forEach(square => {
-            square.style.backgroundColor = '#3498db'; // Reset the background color to white
-          });
+  
+    function createGrid(size) {
+      // Remove previous grid
+      container.innerHTML = "";
+  
+      // Create new grid
+      container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+      for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+      }
+  
+      const rangeSlider = document.querySelector("#range-slider");
+      const sliderValue = document.querySelector("#slider-value");
+  
+      rangeSlider.addEventListener("input", function () {
+        const value = rangeSlider.value;
+        sliderValue.textContent = "Grid Size: " + value + "x" + value;
+      });
+  
+      // Add event listeners to buttons
+      const rainbowButton = document.querySelector("#rainbowButton");
+      rainbowButton.addEventListener("click", function () {
+        // Add 'active' class to rainbow button
+        rainbowButton.classList.add("active");
+        // Remove 'active' class from black button
+        blackButton.classList.remove("active");
+        // Clear the grid back to the default color
+        clearGrid();
+      });
+  
+      const blackButton = document.querySelector("#blackButton");
+      blackButton.addEventListener("click", function () {
+        // Add 'active' class to black button
+        blackButton.classList.add("active");
+        // Remove 'active' class from rainbow button
+        rainbowButton.classList.remove("active");
+        // Clear the grid back to the default color
+        clearGrid();
+      });
+  
+      const squares = document.querySelectorAll(".square");
+      let isDrawing = false; // Create a variable to track if the mouse button is being held down
+  
+      document.addEventListener("mousedown", function () {
+        isDrawing = true; // Set isDrawing to true when mouse button is pressed down
+      });
+  
+      document.addEventListener("mouseup", function () {
+        isDrawing = false; // Set isDrawing to false when mouse button is released
+      });
+  
+      squares.forEach((square) => {
+        square.addEventListener("mousemove", function () {
+          if (isDrawing) {
+            if (rainbowButton.classList.contains("active")) {
+              const randomColor = Math.floor(Math.random() * 16777215).toString(
+                16
+              ); // Generate a random hex color
+              square.style.backgroundColor = "#" + randomColor; // Set the background color
+            } else if (blackButton.classList.contains("active")) {
+              square.style.backgroundColor = "black"; // Set the background color to black
+            }
+          }
+        });
+      });
+  
+      const resetButton = document.querySelector("#resetButton");
+      resetButton.addEventListener("click", function () {
+        // Clear the grid back to the default color
+        clearGrid();
+      });
+  
+      function clearGrid() {
+        squares.forEach((square) => {
+          square.style.backgroundColor = "#3498db"; // Reset the background color to the default color
         });
       }
     }
